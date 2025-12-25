faf323518d9baacfc0670cdb0fc8cd040ec28086
Jacobo Montero Naranjo <151523239+DYeicob@users.noreply.github.com>
2025-12-25 12:01:26 +0100

    Update unet.py

--- src/unet.py
+++ src/unet.py
@@ -51,10 +51,6 @@ class Up(nn.Module):
         x1 = torch.nn.functional.pad(x1, [diffX // 2, diffX - diffX // 2,
                                           diffY // 2, diffY - diffY // 2])
         
-        # if you have padding issues, see
-        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
-        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
-        
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
 
@@ -98,3 +94,4 @@ class UNet(nn.Module):
         x = self.up4(x, x1)
         logits = self.outc(x)
         return self.sigmoid(logits)
+
